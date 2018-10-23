97d527406000ccf1ba5cf609003f55f3c658d7dc
darshankini <44364400+darshankini@users.noreply.github.com>
2018-10-23 21:55:47 +0530

    Add files via upload

---
+++ views/common/aside.php
@@ -0,0 +1,9 @@
+<aside>
+			
+			<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:20%">
+  <h3 class="w3-bar-item">Menu</h3>
+  		<a href="<?php echo base_url('AdminController/manufacturerForm'); ?>" >Add Manufacturer</a><br><br>
+		  <a href="<?php echo base_url('AdminController/modelForm'); ?>" >Add Model</a><br><br>
+			<a href="<?php echo base_url('AdminController/viewInventory'); ?>">View Models</a>
+</div>
+		</aside>
---
+++ views/common/header.php
@@ -0,0 +1,4 @@
+<header>
+		<h2 class="text-center">Mini Car Inventory System</h2>
+	</header>
+<input type="hidden" name="base_url" value="<?php echo base_url(); ?>" id="base_url">
